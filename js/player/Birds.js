a99a2bc7c46dba20b8c4286476780b9244a6e7f7
Evolto <20170907@qq.com>
2019-10-22 15:19:19 +0800

    第三天内容,电脑开发结束
    小鸟的飞翔,结束按钮,得分的绘制,游戏开始结束的逻辑判定

--- js/player/Birds.js
+++ js/player/Birds.js
@@ -31,11 +31,25 @@ export class Birds extends Sprite{
   }
 
   draw(){
+    // 小鸟的状态切换(翅膀方向的切换)
     this.count += 0.2;
     if(this.index>=2){
       this.count=0;
     }
     this.index = Math.floor(this.count);
+
+    //小鸟自由落体运动
+    const g = 0.98/2.4; // 模拟的重力加速度
+
+    //小鸟向上的位移量
+    const Y = 30;
+
+    //小鸟下落的距离
+    const offsetY = (g*this.time*(this.time-Y))/2;
+    for(let i=0;i<3;i++){
+      this.birdsY[i] = this.y[i] + offsetY;
+    }
+    this.time++;
 
     super.draw(
       this.img,
